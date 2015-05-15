f2a6f409d04b9ed4ced7f61e2f2e20e4d97a71d2
Brent Dady <brentdady@gmail.com>
2015-05-15 09:11:04 -0700

    Initial Commit

---
+++ WeekOneAssessment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeekOneAssessment
+//
+//  Created by Brent Dady on 5/15/15.
+//  Copyright (c) 2015 Brent Dady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeekOneAssessment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeekOneAssessment
+//
+//  Created by Brent Dady on 5/15/15.
+//  Copyright (c) 2015 Brent Dady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
